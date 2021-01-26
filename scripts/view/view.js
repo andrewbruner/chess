c8347ab1c8b96a24b098407377098467836e6943
Andrew Bruner <andrewbruner@gmail.com>
2021-01-26 21:34:26 +0000

    Add highlightMove and resetHighlight methods

--- scripts/view/view.js
+++ scripts/view/view.js
@@ -1,6 +1,20 @@
 const view = {
     update() {
-        console.log('view updated!');
+        console.log("view updated!");
+    },
+
+    highlightMove(move) {
+        const notation = move.endSquare.notation;
+        const selector = `[data-notation ="${notation}"]`;
+        const squareDiv = document.querySelector(selector);
+        squareDiv.classList.add("highlight");
+    },
+
+    resetHighlights() {
+        const squareDivs = document.querySelectorAll('.highlight');
+        squareDivs.forEach((squareDiv) => {
+            squareDiv.classList.remove("highlight");
+        });
     }
 };
 
