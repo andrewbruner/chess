18d34d60c561a0420b8fbbcbe1d2670da60d4716
Andrew Bruner <andrewbruner@gmail.com>
2021-01-26 13:20:17 +0000

    Add Move notation

--- scripts/Move.js
+++ scripts/Move.js
@@ -1,35 +1,35 @@
 import chessgame from './script.js';
 
 class Move {
-    constructor(piece, startSquare, endSquare) {
+    constructor(piece, startSquare, endSquare, {
+        willCapture = false,
+        willCheck = false,
+        provokesEnPassant = false,
+        isEnPassant = false,
+        isCastle = false,
+        isPromotion = false
+    } = { }) {
         this.piece = piece;
         this.startSquare = startSquare;
         this.endSquare = endSquare;
+        this.willCapture = willCapture;
+        this.willCheck = willCheck;
+        this.provokesEnPassant = provokesEnPassant;
+        this.isEnPassant = isEnPassant;
+        this.isCastle = isCastle;
+        this.isPromotion = isPromotion;
     }
 
     get notation() {
-        console.log(this.piece.notation, this.endSquare.notation);
-        return this.piece.notation + this.endSquare.notation
-    }
-
-    get willCapture() {
-
-    }
-
-    get willCheck() {
-
-    }
-
-    get isEnPassant() {
-
-    }
-
-    get isCastle() {
-
-    }
-
-    get isPromotion() {
-
+        let notation = this.piece.notation;
+        if (this.willCapture) {
+            if (!this.piece.notation) {
+                notation += this.piece.startSquare.notation.charAt(0);
+            }
+            notation += 'x';
+        }
+        notation += this.endSquare.notation;
+        return notation;
     }
 }
 
