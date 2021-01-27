8683f269609b49df3d4fdafc6ea708ed02ef6bc1
Andrew Bruner <andrewbruner@gmail.com>
2021-01-27 13:08:55 +0000

    Refactor script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
+// import dependencies
 import Game from './model/Game.js';
 import controller from './controller/controller.js';
 
+// start new game
 const chessgame = new Game();
 
-window.chessgame = chessgame;
-
+// add event listener for chessboard clicks
 document.querySelector('.chessboard').addEventListener('click', controller.handleClick);
 
+// enable chessgame access in browser console
+window.chessgame = chessgame;
+
+// export chessgame
 export default chessgame;
